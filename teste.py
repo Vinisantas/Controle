0bebcde080f3f8eaadd4f42038c3d65e500907d1
Vinisantas <viniciuspereirasantana@gmail.com>
2025-06-26 16:39:54 -0300

    chore: update code structure and remove redundant changes

--- teste.py
+++ teste.py
@@ -1,15 +1,17 @@
-from pathlib import Path
 import sqlite3
 
-# Definir o caminho do banco de dados
-caminho = 'cadastro_patrimonio.sqlite'
+# Conectar ao banco de dados
+conn = sqlite3.connect("cadastro_patrimonio.sqlite")
 
-# Verificar se o arquivo existe antes de tentar conectar
-if not Path(caminho).exists():
-    print("Arquivo de banco de dados não encontrado.")
-else:
-    # Conectar ao banco de dados se o arquivo existir
-    conn = sqlite3.connect(caminho)
-    print("Conexão estabelecida com sucesso.")
-    # Fechar a conexão
-    conn.close()
+# Listar as tabelas existentes
+cursor = conn.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+tables = cursor.fetchall()
+
+# Exibir as tabelas
+print("Tabelas no banco de dados:")
+for table in tables:
+    print(table[0])
+
+# Fechar a conexão
+conn.close()
